48eae593a54ae973203fda289012b65d0ec33b66
dfklein <dklein@gmail.com>
2018-10-12 19:35:55 -0300

    Commit genérico

--- Persistencia/02JavaEJPAPersistaSeusObjetosComAJPA2EHibernate/src/br/com/caelum/financas/teste/TesteConta.java
+++ Persistencia/02JavaEJPAPersistaSeusObjetosComAJPA2EHibernate/src/br/com/caelum/financas/teste/TesteConta.java
@@ -1,9 +1,34 @@
 package br.com.caelum.financas.teste;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.caelum.financas.modelo.Conta;
+
 public class TesteConta {
 
 	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.setTitular("Denis");
+		conta.setBanco("Itau");
+		conta.setAgencia("0786");
+		conta.setNumero("460786-6");
 		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
+		EntityManager em = emf.createEntityManager();
+		
+		// Inicia a transação
+		em.getTransaction().begin();
+		
+		em.persist(conta);
+		
+		// Commit
+		em.getTransaction().commit();
+		
+		// Fecha a conexão com o banco de dados para liberar recursos utilizados.
+		em.close();
+		emf.close();
 	}
 	
 }
