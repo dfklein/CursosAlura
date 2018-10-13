8c42fd0267fa07da5bf55379878659a8f63ec5c0
dfklein <dklein@gmail.com>
2018-10-12 22:18:37 -0300

    Aula 05

---
+++ Persistencia/02JavaEJPAPersistaSeusObjetosComAJPA2EHibernate/src/br/com/caelum/financas/teste/TesteJPARelacionamento.java
@@ -0,0 +1,41 @@
+package br.com.caelum.financas.teste;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteJPARelacionamento {
+
+	public static void main(String[] args) {
+		
+		Conta ct = new Conta();
+		ct.setAgencia("0102");
+		ct.setBanco("Itaú");
+		ct.setNumero("123");
+		ct.setTitular("Leonardo");
+		
+		Movimentacao mv = new Movimentacao();
+		mv.setData(Calendar.getInstance());
+		mv.setDescricao("Churrascaria");
+		mv.setTipo(TipoMovimentacao.SAIDA);
+		mv.setValor(new BigDecimal("200.0"));
+		mv.setConta(ct);
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+		
+		em.getTransaction().begin();
+		
+		em.persist(ct); 
+		em.persist(mv); 
+		
+		em.getTransaction().commit();
+		
+		em.close();
+	}
+}
