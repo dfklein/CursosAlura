a0a146cfad69eb45cb441da8f686309b92aca3a2
dfklein <dklein@gmail.com>
2018-10-22 23:31:14 -0300

    Aula 01

---
+++ JavaParaWeb/01CursoServletParte1FundamentosDaProgramaçãoWebJava/src/br/com/gerenciador/servlet/OiMundoServelet.java
@@ -0,0 +1,27 @@
+package br.com.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/oi")
+public class OiMundoServelet extends HttpServlet {
+
+	private static final long serialVersionUID = 3665823450861138203L;
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Primeiro servlet!</h1>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+
+}
