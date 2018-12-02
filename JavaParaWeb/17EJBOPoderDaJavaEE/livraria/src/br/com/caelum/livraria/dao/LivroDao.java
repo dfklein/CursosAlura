4f92866dfd9915aa42d4e56ace3cb781c1d0b0ee
dfklein <dklein@gmail.com>
2018-12-02 11:27:01 -0200

    Aula 03

--- JavaParaWeb/17EJBOPoderDaJavaEE/livraria/src/br/com/caelum/livraria/dao/LivroDao.java
+++ JavaParaWeb/17EJBOPoderDaJavaEE/livraria/src/br/com/caelum/livraria/dao/LivroDao.java
@@ -3,21 +3,30 @@ package br.com.caelum.livraria.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import br.com.caelum.livraria.modelo.Livro;
 
 
 @Stateless
 public class LivroDao {
+	
+	@PersistenceContext(name="livraria")
+	private EntityManager manager;
 
-	private Banco banco = new Banco();
+//	private Banco banco = new Banco();
 	
 	public void salva(Livro livro) {
-		banco.save(livro);
+		manager.persist(livro);
+//		banco.save(livro);
 	}
 	
 	public List<Livro> todosLivros() {
-		return banco.listaLivros();
+		TypedQuery<Livro> query = manager.createQuery("SELECT l FROM Livro l", Livro.class);
+		return query.getResultList();
+//		return banco.listaLivros();
 	}
 	
 }
--- JavaParaWeb/17EJBOPoderDaJavaEE/livraria/src/br/com/caelum/livraria/dao/UsuarioDao.java
+++ JavaParaWeb/17EJBOPoderDaJavaEE/livraria/src/br/com/caelum/livraria/dao/UsuarioDao.java
@@ -1,17 +1,26 @@
 package br.com.caelum.livraria.dao;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import br.com.caelum.livraria.modelo.Usuario;
 
 
 @Stateless
 public class UsuarioDao {
+	
+	@PersistenceContext(name="livraria")
+	private EntityManager manager;
 
-	private Banco banco = new Banco();
+//	private Banco banco = new Banco();
 
 	public Usuario buscaPeloLogin(String login) {
-		return this.banco.buscaPeloNome(login);
+		TypedQuery<Usuario> query = manager.createQuery("SELECT u FROM Usuario u WHERE u.login = :login", Usuario.class);
+		query.setParameter("login", login);
+		return query.getSingleResult();
+//		return this.banco.buscaPeloNome(login);
 	}
 	
 }
