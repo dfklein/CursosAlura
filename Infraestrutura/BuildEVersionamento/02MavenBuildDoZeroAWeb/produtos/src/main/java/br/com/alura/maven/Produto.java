6ff18d390ded6eb303377bbb45739c05d7e62cc7
Denis Frederico Klein <dfrederi@everis.com>
2019-01-18 18:16:27 -0200

    Aula 01 (em andamento)

---
+++ Infraestrutura/BuildEVersionamento/02MavenBuildDoZeroAWeb/produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -0,0 +1,13 @@
+package br.com.alura.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class Produto 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Classe produto" );
+    }
+}
