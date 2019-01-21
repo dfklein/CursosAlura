cc55a21959f4aec8acb7430dceed29ecf17acdb1
Denis Frederico Klein <dfrederi@everis.com>
2019-01-21 16:33:06 -0200

    Aula 02

--- Infraestrutura/BuildEVersionamento/02MavenBuildDoZeroAWeb/produtos/src/main/java/br/com/alura/maven/Produto.java
+++ Infraestrutura/BuildEVersionamento/02MavenBuildDoZeroAWeb/produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -4,10 +4,31 @@ package br.com.alura.maven;
  * Hello world!
  *
  */
-public class Produto 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Classe produto" );
-    }
+public class Produto {
+	
+	private String nome;
+	private double preco;
+	
+	public Produto(String nome, double preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+   
 }
