28a6f32ba512467746f6878a1a3bee7629470590
Denis Frederico Klein <dfrederi@everis.com>
2019-01-29 17:21:41 -0200

    Aula 04

---
+++ Java/09Java8TireProveitoDosNovosRecursosDaLinguagem/java8/src/br/com/alura/java8/C04Streams.java
@@ -0,0 +1,92 @@
+package br.com.alura.java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class C04Streams {
+
+	public static void main(String[] args) {
+		List<Curso> cursos = new ArrayList<Curso>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		cursos.sort(Comparator.comparing(Curso::getNome));
+		System.out.println("Foi criada e ordenada por nome esta lista: ");
+		cursos.forEach(c -> System.out.println(c.getNome() + " - " + c.getAlunos()));
+		
+		System.out.println("\n");
+		
+		// Filtro: somente cursos com 100 alunos ou mais.
+		cursos.stream()
+			.filter(c -> c.getAlunos() >= 100);
+		
+		// Quando você utiliza uma stream ela trabalha sobre um novo objeto sem impactar a lista original.
+		// Se você executar a linha abaixo, verá que o filtro NÃO fez efeito:
+		System.out.println("Lendo os nomes da lista depois de filtrar a stream (provando que isto não afeta a lista original):");
+		cursos.forEach(c -> System.out.println(c.getNome() + " - " + c.getAlunos()));
+		
+		System.out.println("\n");
+		
+		// As streams também possuem um forEach:
+		System.out.println("Iterando a partir de um forEach da própria stream e fazendo o filtro funcionar:");
+		cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.forEach(c -> System.out.println(c.getNome() + " - " + c.getAlunos()));
+		
+		System.out.println("\n");
+		
+		System.out.println("Mapeando o número de alunos de cada curso retornando um objeto Integer:");
+		cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.map(Curso::getAlunos)
+			.forEach(c -> System.out.println(c + " - " + c.getClass()));
+		
+		System.out.println("\n");
+		
+		System.out.println("Mapeando o número de alunos de cada curso retornando um int (tipo primitivo):");
+		
+		/**
+		 * Quando você retorna um tipo primitivo (como o mapToInt) você possui métodos específicos que
+		 * permitem você trabalhar com eles (no exemplo acima em que você mapeou como objeto Integer
+		 * isto não existe pois é uma stream de Object.
+		 */
+		int sum = cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.mapToInt(Curso::getAlunos)
+			.sum();
+		System.out.println(sum);
+		
+		System.out.println("\n");
+		
+		/**
+		 * Estes métodos de stream que trabalham com tipos primitivos são interessantes em termos de
+		 * performance pois ele sabe trabalhar sem realizar tantas operações de boxing e unboxing.
+		 * 
+		 * Abaixo um método específico de ordenação em que isso seria válido
+		 */
+		System.out.println("Ordenando por qtd de alunos com um método específico para tipo primitivos:");
+		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+		cursos.forEach(c -> System.out.println(c.getNome() + " - " + c.getAlunos()));
+	}
+}
+
+class Curso {
+    private String nome;
+    private int alunos;
+
+    public Curso(String nome, int alunos) {
+        this.nome = nome;
+        this.alunos = alunos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAlunos() {
+        return alunos;
+    }
+}
