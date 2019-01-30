6f42313890e56018b0ca6aad4276f13d36c4746a
Denis Frederico Klein <dfrederi@everis.com>
2019-01-30 11:56:30 -0200

    Aula 03

--- JavaParaWeb/16SpringBootAgilidadeNoDesenvolvimentoJavaComSpring/listavip/src/main/java/br/com/alura/listavip/controller/ConvidadoController.java
+++ JavaParaWeb/16SpringBootAgilidadeNoDesenvolvimentoJavaComSpring/listavip/src/main/java/br/com/alura/listavip/controller/ConvidadoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.alura.listavip.model.Convidado;
 import br.com.alura.listavip.repository.ConvidadoRepository;
@@ -17,11 +19,16 @@ public class ConvidadoController {
 	@RequestMapping("/")
 	public String index() {
 		// Observe que apenas por adotar a convenção do Spring Boot (colocar seus templates na pasta
-		// resources) você não precisou fazer todas aquelas configurações de servlet que você fez nos
+		// resources) vocÃª nÃ£o precisou fazer todas aquelas configurações de servlet que você fez nos
 		// cursos de mvc para retornar o nome do arquivo da página de resposta da requisição.
 		return "index";
 	}
 	
+	/**
+	 * Aparentemente toda requisição do Spring Boot possui um objeto da interface Model que pode ser
+	 * recebido como argumento. O que você adicionar a ele é atachado ao response da requisição para
+	 * ser utilizado na tela.
+	 */
 	@RequestMapping("listaconvidados")
 	public String listaConvidados(Model model){
 		Iterable<Convidado> convidados = repository.findAll();
@@ -30,5 +37,20 @@ public class ConvidadoController {
 		model.addAttribute("convidados", convidados);
 		
 	    return "listaconvidados";
+	}
+	
+	@RequestMapping(value= "salvar", method = RequestMethod.POST)
+	public String salvar(@RequestParam("nome") String nome, @RequestParam("email") String email, 
+            @RequestParam("telefone") String telefone, Model model ){
+		
+		Convidado novoConvidado = new Convidado(nome, email, telefone);
+	    repository.save(novoConvidado);
+
+	    // No curso ele fez o código abaixo. Eu achei tosco e por isso fiz um redirect
+	    // O objeto Model que esse método recebe como argumento serve só pra isso.
+		//	    Iterable<Convidado> convidados = repository.findAll();
+		//	    model.addAttribute("convidados", convidados);
+	    return "redirect:listaconvidados";
+
 	}
 }
