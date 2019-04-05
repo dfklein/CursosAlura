e169e451f295f317d44639acdb6ec1ec025c1da3
Denis Frederico Klein <dfrederi@everis.com>
2019-04-05 12:59:39 -0300

    Aula 03

--- Front-End/Frameworks MVC/06ReactParte1ComponentesReutilizaveisParaSuaWebapp/cdc-admin/src/App.js
+++ Front-End/Frameworks MVC/06ReactParte1ComponentesReutilizaveisParaSuaWebapp/cdc-admin/src/App.js
@@ -1,80 +1,117 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import $ from 'jquery';
 
 class App extends Component {
-  render() {
-    return (
-<div id="layout">
 
-    <a href="#menu" id="menuLink" className="menu-link">
+    // Esta função é o construtor do componente. Ideal para a inicialização do mesmo
+    constructor() {
+        // Sem a declaração explícita do super() o restante do código apresentará erro de compilação.
+        super();
+        // a variável state é a que guarda o estado desta classe. O React só reconhece ela com esta
+        // função. Ou seja: todas as variáveis que guardam estado da sua view são declarados dentro dele.
+        this.state = { lista: [] };
 
-        <span></span>
-    </a>
+        // É considerado má prática fazer chamadas ajax no construtor do seu componente
+    }
 
-    <div id="menu">
-        <div className="pure-menu">
-            <a className="pure-menu-heading" href="#">Company</a>
+    // Função chamada após o construtor mas antes da renderização do componente
+    componentWillMount() { }
 
-            <ul className="pure-menu-list">
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+    // Esta função é chamada logo após a renderização do seu componente (após o render())
+    componentDidMount() {
+        console.log("didMount");
+        $.ajax({
+            url: "http://localhost:8080/api/autores",
+            dataType: 'json',
+            success: function (resposta) {
+                console.log("chegou a resposta");
+                this.setState({ lista: resposta });
+            }.bind(this)
+        }
+        );
+    }
 
 
-            </ul>
-        </div>
-    </div>
+    render() {
+        return (
+            <div id="layout">
 
-        <div id="main">
-            <div className="header">
-              <h1>Cadastro de Autores</h1>
+                <a href="#menu" id="menuLink" className="menu-link">
+
+                    <span></span>
+                </a>
+
+                <div id="menu">
+                    <div className="pure-menu">
+                        <a className="pure-menu-heading" href="#">Company</a>
+
+                        <ul className="pure-menu-list">
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+
+
+                        </ul>
+                    </div>
+                </div>
+
+                <div id="main">
+                    <div className="header">
+                        <h1>Cadastro de Autores</h1>
+                    </div>
+                    <div className="content" id="content">
+                        <div className="pure-form pure-form-aligned">
+                            <form className="pure-form pure-form-aligned">
+                                <div className="pure-control-group">
+                                    <label htmlFor="nome">Nome</label>
+                                    <input id="nome" type="text" name="nome" value="" />
+                                </div>
+                                <div className="pure-control-group">
+                                    <label htmlFor="email">Email</label>
+                                    <input id="email" type="email" name="email" value="" />
+                                </div>
+                                <div className="pure-control-group">
+                                    <label htmlFor="senha">Senha</label>
+                                    <input id="senha" type="password" name="senha" />
+                                </div>
+                                <div className="pure-control-group">
+                                    <label></label>
+                                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                                </div>
+                            </form>
+
+                        </div>
+                        <div>
+                            <table className="pure-table">
+                                <thead>
+                                    <tr>
+                                        <th>Nome</th>
+                                        <th>email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.lista.map(function (autor) {
+                                            return (
+                                                <tr key={autor.id}>
+                                                    <td>{autor.nome}</td>
+                                                    <td>{autor.email}</td>
+                                                </tr>
+                                            );
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
-            <div className="content" id="content">
-              <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label> 
-                    <input id="nome" type="text" name="nome" value=""  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label> 
-                    <input id="email" type="email" name="email" value=""  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label> 
-                    <input id="senha" type="password" name="senha"  />                                      
-                  </div>
-                  <div className="pure-control-group">                                  
-                    <label></label> 
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                  </div>
-                </form>             
-
-              </div>  
-              <div>            
-                <table className="pure-table">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Alberto</td>                
-                      <td>alberto.souza@caelum.com.br</td>                
-                    </tr>
-                  </tbody>
-                </table> 
-              </div>             
-            </div>
-          </div>            
-
-
-</div>     
-    );
-  }
+        );
+    }
 }
 
 export default App;
