f29ee80127f7c43a21f780b172ccc10f73b274a0
Denis Frederico Klein <dfrederi@everis.com>
2019-05-03 20:36:07 -0300

    Aula 2 - não concluída

--- Front-End/Frameworks_MVC/02AngularParte1Fundamentos/alurapic/src/app/app.component.ts
+++ Front-End/Frameworks_MVC/02AngularParte1Fundamentos/alurapic/src/app/app.component.ts
@@ -7,6 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
-  titlepic='Silly Walk';
-  url='https://i.etsystatic.com/5840489/r/il/971ca4/402886681/il_794xN.402886681_4m08.jpg';
+  
 }
---
+++ Front-End/Frameworks_MVC/02AngularParte1Fundamentos/alurapic/src/app/foto/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+
+    titlepic = 'Silly Walk';
+    url = 'https://i.etsystatic.com/5840489/r/il/971ca4/402886681/il_794xN.402886681_4m08.jpg';
+}
