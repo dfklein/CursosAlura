397692fc03ad0bcd8b5c6dbe747e526cfce293ff
Denis Frederico Klein <dfrederi@everis.com>
2019-05-03 19:18:48 -0300

    Aula 01

---
+++ Front-End/Frameworks_MVC/02AngularParte1Fundamentos/alurapic/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alurapic';
+  titlepic='Silly Walk';
+  url='https://i.etsystatic.com/5840489/r/il/971ca4/402886681/il_794xN.402886681_4m08.jpg';
+}
