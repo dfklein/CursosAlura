13115a92815a9ec1abde501a1ef8b7d2a732413d
dfklein <dklein@gmail.com>
2019-05-06 22:57:07 -0300

    Aula 04 (metade)

--- Front-End/Frameworks_MVC/02AngularParte1Fundamentos/alurapic/src/app/photos/photos.module.ts
+++ Front-End/Frameworks_MVC/02AngularParte1Fundamentos/alurapic/src/app/photos/photos.module.ts
@@ -3,6 +3,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { CommonModule } from '@angular/common';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
     // Ao criar um módulo você precisa declarar quais componentes fazem parte deste módulo.
@@ -10,12 +12,20 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
     // declarados aqui dentro são acessíveis apenas entre si
     declarations: [
         PhotoComponent,
-        PhotoListComponent
+        PhotoListComponent,
+        PhotoFormComponent
     ],
     // Se o módulo que você está fazendo for utilizado por outros módulos, você precisa exportá-lo
     // Ao exportar o componente ele passa a ser visível de fora deste módulo  
     
-    imports: [ HttpClientModule ]
+    imports: [ 
+        HttpClientModule, 
+        CommonModule    // O common module é um módulo que faz parte do BrowserModule que é declarado em app.module.ts.
+                        // O BrowserModule é um módulo que SÓ PODE SER DECLARADO no app.module e que possui mecanismos
+                        // próprios do Angular para rodar. Porém é ele também quem possui as diretivas, sendo necessário
+                        // em outros lugares. Desta forma, nestes outros lugares, você pode importar o CommonModule
+                        // para obter estes recursos como as diretivas.
+    ]
 })
 export class PhotosModule {
 
