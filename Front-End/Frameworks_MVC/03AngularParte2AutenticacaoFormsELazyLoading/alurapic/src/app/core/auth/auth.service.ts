8b8558517dfe75f0a5e7ac7a00d7d9ca8e116141
Denis Frederico Klein <dfrederi@everis.com>
2019-05-14 17:24:29 -0300

    Aula 02

--- Front-End/Frameworks_MVC/03AngularParte2AutenticacaoFormsELazyLoading/alurapic/src/app/core/auth/auth.service.ts
+++ Front-End/Frameworks_MVC/03AngularParte2AutenticacaoFormsELazyLoading/alurapic/src/app/core/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -8,11 +10,25 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService) { }
 
   authenticate(userName:string, password:string) {
     // this.http.post(API_URL + '/user/login', {username: userName, password: password});
     // Lembre que quando você está passando num parâmetro uma propriedade que tem o mesmo nome de uma variável você pode passar da seguinte forma:
-    return this.http.post(API_URL + '/user/login', {userName, password});
+    return this.http
+      // o terceiro argumento de post ({ observe : 'response'}) é passado como forma de você ter acesso
+      // a toda a resposta gerada. Sem isso você não tem acesso a nada, incluindo o header de onde você
+      // precisa tirar o token.
+      .post(API_URL + '/user/login', {userName, password}, { observe : 'response'})
+      // Este pipe e o operador tap garantem que um código será executado antes do método retornar
+      // a resposta da requisição para a classe que chamou o serviço.
+      .pipe(tap(
+          res => {
+            const authToken = res.headers.get('x-access-token');
+            this.tokenService.setToken(authToken);
+          }
+      ));
   }
 }
---
+++ Front-End/Frameworks_MVC/03AngularParte2AutenticacaoFormsELazyLoading/alurapic/src/app/core/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl:'./header.component.html',
+    selector:'ap-header'
+
+})
+export class HeaderComponent {
+
+}
