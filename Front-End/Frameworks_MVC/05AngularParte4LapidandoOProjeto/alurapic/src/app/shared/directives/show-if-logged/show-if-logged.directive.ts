faad85610222d68887ae2cba2789edbcf3c7679a
Denis Frederico Klein <dfrederi@everis.com>
2019-05-25 15:14:38 -0300

    Aula 04

--- Front-End/Frameworks_MVC/05AngularParte4LapidandoOProjeto/alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
+++ Front-End/Frameworks_MVC/05AngularParte4LapidandoOProjeto/alurapic/src/app/shared/directives/show-if-logged/show-if-logged.directive.ts
@@ -7,6 +7,8 @@ import { UserService } from "../../../core/user/user.service";
     selector: '[showIfLogged]'
 })
 export class ShowIfLoggedDirective implements OnInit { 
+
+    currentDisplay: string;
     
     constructor(
         private element: ElementRef<any>,
@@ -15,7 +17,28 @@ export class ShowIfLoggedDirective implements OnInit {
     ) {}
 
     ngOnInit(): void {
-        !this.userService.isLogged() 
-            && this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+
+        // O mecanismo abaixo é para fazer com que a diretiva funcione em elementos mesmo que
+        // não sejam recarregados. 
+        // Havia um bug no menu (hamburguer) que possuía elementos que não podiam ser visíveis
+        // caso o usuário estivesse deslogado. Como ao dar logout o header não era recarregado,
+        // o ítem continuava visível mesmo com o usuário deslogado.
+        // Ele funciona se inscrevendo para ouvir alterações de usuário em toda a aplicação para
+        // reexecutar a lógica de exibição de ítens quando isto acontece.
+        // 1 - Guarda o estilo atual do elemento que usa esta diretiva em uma string.
+        this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+        this.userService.getUser().subscribe(user => {
+            if(user) {
+                // 2 - Se tem usuário logado, mantém o estilo atual do elemento.
+                this.renderer.setElementStyle(this.element.nativeElement, 'display', this.currentDisplay);
+            } else {
+                this.currentDisplay = getComputedStyle(this.element.nativeElement).display;
+                this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
+            }
+        })
+
+        // Versão anterior desta função.
+        // !this.userService.isLogged() 
+        //    && this.renderer.setElementStyle(this.element.nativeElement, 'display', 'none');
     }
 }
