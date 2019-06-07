8dff8d6d32454795d5ebc7113ada783f7d9ec92a
Denis Frederico Klein <dfrederi@everis.com>
2019-06-07 12:45:00 -0300

    Aula 05

--- Programacao/Python/02Python3parte1IntroducaoANovaVersaoDaLinguagem/jogos/adivinhacao.py
+++ Programacao/Python/02Python3parte1IntroducaoANovaVersaoDaLinguagem/jogos/adivinhacao.py
@@ -3,13 +3,34 @@ print("Bem vindo ao jogo de advinhação")
 print("*******************************")
 
 numero_secreto = 42
-palpite = input("Digite o seu número: ")
+total_de_tentativas = 3
 
-num_palpite = int(palpite)
-print("Você digitou", num_palpite)
 
-if (numero_secreto == num_palpite):
-    print("Você acertou!")
-else:
-    print("Você errou!")
+for rodada in range(1, total_de_tentativas + 1):    # você pode definir um terceiro argumento no range que seria o fator de
+                                                    # de incremento (por exemplo: se for 2 ele somará de dois em dois).
+
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    palpite = input("Digite o seu número: ")
+    print("Você digitou", chute)
+    chute = int(palpite)
+
+    if(chute < 1 or chute > 100):
+        print("O valor precisa estar entre 1 e 100")
+        continue
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+        break
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+
+
 print("Fim do jogo")
