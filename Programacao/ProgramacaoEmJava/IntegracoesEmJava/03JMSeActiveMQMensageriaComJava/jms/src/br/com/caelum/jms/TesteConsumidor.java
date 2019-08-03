4e154db9f4d6697b7d6b3ba8968592192bfc0f1c
dfklein <dklein@gmail.com>
2019-08-03 16:17:37 -0300

    Aula 03

--- Programacao/ProgramacaoEmJava/IntegracoesEmJava/03JMSeActiveMQMensageriaComJava/jms/src/br/com/caelum/jms/TesteConsumidor.java
+++ Programacao/ProgramacaoEmJava/IntegracoesEmJava/03JMSeActiveMQMensageriaComJava/jms/src/br/com/caelum/jms/TesteConsumidor.java
@@ -8,7 +8,9 @@ import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
@@ -39,15 +41,35 @@ public class TesteConsumidor {
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);	// O primeiro argumento é para dizer se a sessão é transacional ou não. Se for, você poderá chamar um método commit() 
 		
 		// Localiza a fila que se deseja consumir.
-		Destination fila = (Destination) context.lookup("financeiro");
+		Destination fila = (Destination) context.lookup("financeiro"); // veja no arquivo jndi.properties que "financeiro" refere-se a uma fila registrada no ActiveMQ
 		MessageConsumer consumer = session.createConsumer(fila);
 		
-		Message message = consumer.receive(); // obtem a mensagem da fila.
-		System.out.println("Mensagem: " + message);
-		new Scanner(System.in).nextLine(); // parar o programa para testar a conexao
+		// Message message = consumer.receive(2000); 	// obtem a mensagem da fila, mas é melhor criar um listener para que ele não
+														// consuma apenas a primeira mensagem da fila.
+		
+		consumer.setMessageListener(new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				try {
+					TextMessage textMessage = (TextMessage) message;
+				
+					System.out.println("Mensagem: " + textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		
+		 new Scanner(System.in).nextLine(); // parar o programa para testar a conexao
 
 		session.close();
 		connection.close();
 		context.close();
+		System.out.println("fim");
 	}
 }
